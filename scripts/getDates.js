2eaf078a06214cbdda3ea1805383a244b596307a
sydrickbissombolo <newtonbissombolo2@gmail.com>
2023-09-11 11:08:54 +0100

    first assignment

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,1 +1,5 @@
-document.getElementById('currentYear').textContent = new Date().getFullYear();
+const currentYearSpan = document.getElementById("currentYear");
+const currentYear = new Date().getFullYear();
+const lastModifiedParagraph = document.getElementById("lastModified");
+const lastModified = new Date(document.lastModified);
+lastModifiedParagraph.textContent = "Last Modified: " + lastModified.toLocaleString();
