6315156b420c992b4c0a40b1db3b8f6ac578f293
sydrickbissombolo <newtonbissombolo2@gmail.com>
2023-09-29 10:40:10 +0100

    Added a google map

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,11 @@ const currentYear = new Date().getFullYear();
 const lastModifiedParagraph = document.getElementById("lastModified");
 const lastModified = new Date(document.lastModified);
 lastModifiedParagraph.textContent = "Last Modified: " + lastModified.toLocaleString();
+
+
+const menuToggle = document.querySelector(".menu-toggle");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+menuToggle.addEventListener("click", () => {
+    mobileMenu.classList.toggle("show");  // toggle the show class
+})
