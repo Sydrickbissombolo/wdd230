8c27dca3415df619d7ab4a411639e1bb5ac9b2b4
sydrickbissombolo <newtonbissombolo2@gmail.com>
2023-11-18 08:44:45 +0100

    api weather for the chamber of commerce

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -113,44 +113,75 @@ function toggleView(viewType) {
 
 document.addEventListener("DOMContentLoaded", function () {
   const apiKey = "eac1da47236a8fe587a56e2522f38bd7";
-  const city = "Brazzaville";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const chamberLocation = "Dolisie";
+  const apiUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${chamberLocation}&appid=${apiKey}&units=metric`;
+  const apiUrlForecast = `https://api.openweathermap.org/data/2.5/onecall?lat={LATITUDE}&lon={LONGITUDE}&exclude=current,minutely,hourly&appid=${apiKey}&units=metric`;
 
-  fetch(apiUrl)
+  // Checking if today is Monday, Tuesday, or Wednesday
+  const today = new Date().getDay();
+  const isBannerDay = today >= 1 && today <= 3;
+
+  // Displaying or hiding the banner
+  const banner = document.getElementById('banner');
+  const closeBannerButton = document.getElementById('closeBanner');
+
+  if (isBannerDay) {
+      banner.style.display = 'block';
+  }
+
+  closeBannerButton.addEventListener('click', function () {
+      banner.style.display = 'none';
+  });
+
+  // Fetch current weather data
+  fetch(apiUrlCurrent)
       .then(response => response.json())
       .then(data => {
-          displayWeather(data);
+          displayCurrentWeather(data);
       })
       .catch(error => {
-          console.error('Error fetching weather data:', error);
+          console.error('Error fetching current weather data:', error);
+      });
+
+  // Fetch forecast data
+  fetch(apiUrlForecast)
+      .then(response => response.json())
+      .then(data => {
+          displayForecast(data);
+      })
+      .catch(error => {
+          console.error('Error fetching forecast data:', error);
       });
 });
 
-function displayWeather(data) {
-  const weatherIconContainer = document.getElementById('weather-icon');
+function displayCurrentWeather(data) {
+  // Display current weather information
+  const locationElement = document.getElementById('location');
+  const weatherIconElement = document.getElementById('weather-icon');
   const temperatureElement = document.getElementById('temperature');
-  const humidityElement = document.getElementById('humidity');
-  const windSpeedElement = document.getElementById('wind-speed');
-  const windChillElement = document.getElementById('windChill');
+  const weatherConditionElement = document.getElementById('weather-condition');
 
-  const temperature = data.main.temp;
-  const humidity = data.main.humidity;
-  const windSpeed = data.wind.speed;
+  const weatherIconUrl = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
 
-  weatherIconContainer.innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="Weather Icon">`;
-
-  temperatureElement.textContent = `${temperature} °C`;
-  humidityElement.textContent = `${humidity}%`;
-  windSpeedElement.textContent = `${windSpeed}mph`;
-
-  // Calculate wind chill if temperature and wind speed are available
-  if (temperature !== undefined && windSpeed !== undefined) {
-      const windChill = calculateWindChill(temperature, windSpeed);
-      windChillElement.textContent = `${windChill} °C`;
-  }
+  locationElement.textContent = data.name + ', ' + data.sys.country;
+  weatherIconElement.innerHTML = `<img src="${weatherIconUrl}" alt="Weather Icon">`;
+  temperatureElement.textContent = `${data.main.temp} °C`;
+  weatherConditionElement.textContent = data.weather[0].description;
 }
 
-function calculateWindChill(temperature, windSpeed) {
-  // Formula to calculate wind chill
-  return Math.round(13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16));
+function displayForecast(data) {
+  // Display three-day forecast
+  const forecastElement = document.getElementById('forecast');
+  const forecastData = data.daily.slice(1, 4); // Extracting the next three days
+
+  forecastData.forEach(day => {
+      const date = new Date(day.dt * 1000).toLocaleDateString('en-US', { weekday: 'long' });
+      const tempMin = day.temp.min.toFixed(1);
+      const tempMax = day.temp.max.toFixed(1);
+
+      const forecastItem = document.createElement('div');
+      forecastItem.innerHTML = `<p>${date}</p><p>Min: ${tempMin} °C | Max: ${tempMax} °C</p>`;
+
+      forecastElement.appendChild(forecastItem);
+  });
 }
